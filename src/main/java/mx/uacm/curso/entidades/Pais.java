b6e53e9a09de5bc81438a6c613a84057fbc3014e
U-Lee <hecotlaloc@gmail.com>
2020-05-10 00:41:35 -0500

    Mapeo de entidades

--- src/main/java/mx/uacm/curso/entidades/Pais.java
+++ src/main/java/mx/uacm/curso/entidades/Pais.java
@@ -74,4 +74,6 @@ public class Pais {
     public void setLongitud(Double longitud) {
         this.longitud = longitud;
     }
+    
+    
 }
--- src/main/java/mx/uacm/curso/entidades/Usuario.java
+++ src/main/java/mx/uacm/curso/entidades/Usuario.java
@@ -6,27 +6,31 @@
 package mx.uacm.curso.entidades;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="usuarios")
 public class Usuario {
-    
-    @Id
-    @Column(name = "id")
-    @SequenceGenerator(name = "sec_usu", sequenceName = "usuarios_id_seq", allocationSize = 1)
-    @GeneratedValue(generator = "sec_usu", strategy = GenerationType.SEQUENCE)
-    private Integer id;
-    
-    @Column(name = "usuario_twitter")
-    private String usuario_twitter;
-    
-    @Column(name= "siguiendo")
-    private Integer siguiendo;
-    
-    @Column(name="seguidores")
-    private Integer seguidores;
+   
+   @Id
+   @Column(name="id")
+   @SequenceGenerator(name = "sec_usu", sequenceName = "usuarios_id_seq", allocationSize = 1)
+   @GeneratedValue(generator = "sec_usu", strategy = GenerationType.SEQUENCE)
+   private Integer id;
+   
+   @Column(name="usuario_twitter")
+   private String usuario_twitter;
+   
+   @Column(name="siguiendo")
+   private Integer siguiendo;
+   
+   @Column(name="seguidores")
+   private Integer seguidores;
 
     public Integer getId() {
         return id;
@@ -58,8 +62,5 @@ public class Usuario {
 
     public void setSeguidores(Integer seguidores) {
         this.seguidores = seguidores;
-    }
-    
-    
-    
+    }   
 }
